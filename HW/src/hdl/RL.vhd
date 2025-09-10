018d62ef49876268a23a385e613c2262e4a714da
LucasCmb <34749734+lucasCmb-eie@users.noreply.github.com>
2025-09-10 13:36:21 -0300

    Correcion en RL

--- HW/src/hdl/RL.vhd
+++ HW/src/hdl/RL.vhd
@@ -1,24 +1,3 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 19.08.2025 18:38:42
--- Design Name: 
--- Module Name: RL_Load - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.fixed_pkg.all;
@@ -53,41 +32,37 @@ architecture Behavioral of RL is
     -- Señales para los productos intermedios
     constant PROD_INT_BITS  : integer := 2*INT_BITS;
     constant PROD_FRAC_BITS : integer := 2*FRAC_BITS;
-    signal term_a0_v_n   : sfixed(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
-    signal term_a1_v_n_1 : sfixed(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
-    signal term_b1_i_n_1 : sfixed(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
-
     -- Registro para la salida
     signal i_n_reg : sfixed(INT_BITS-1 downto -FRAC_BITS) := (others => '0');
 
 begin
 
     process (i_clk, i_rst)
+        variable term_a0_v_n   : sfixed(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
+        variable term_a1_v_n_1 : sfixed(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
+        variable term_b1_i_n_1 : sfixed(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
+        variable i_n_next      : sfixed(INT_BITS-1 downto -FRAC_BITS);
     begin
-
         if i_rst = '1' then
             v_n_minus_1 <= (others => '0');
             i_n_minus_1 <= (others => '0');
             i_n_reg <= (others => '0');
-
         elsif rising_edge(i_clk) then
-            
-            -- 1. Realizar las multiplicaciones
-            term_a0_v_n   <= i_c_a0 * i_Vi;
-            term_a1_v_n_1 <= i_c_a1 * v_n_minus_1;
-            term_b1_i_n_1 <= i_c_b1 * i_n_minus_1;
+            -- Multiplicaciones usando variables
+            term_a0_v_n   := i_c_a0 * i_Vi;
+            term_a1_v_n_1 := i_c_a1 * v_n_minus_1;
+            term_b1_i_n_1 := i_c_b1 * i_n_minus_1;
 
-            -- 2. Sumar los términos (i[n] = a0*v[n] + a1*v[n-1] - b1*i[n-1])
-            -- La resta se implementa sumando el negado del coeficiente. Como C_B1 es negativo, usamos la resta.
-            -- resize() se encarga del re-escalado y ajuste de tamaño a nuestro formato original.
-            i_n_reg <= resize(term_a0_v_n + term_a1_v_n_1 - term_b1_i_n_1, INT_BITS, -FRAC_BITS);
+            -- Suma y resta
+            i_n_next := resize(term_a0_v_n + term_a1_v_n_1 - term_b1_i_n_1, INT_BITS-1, -FRAC_BITS);
 
-            -- 3. Actualizar los registros de retardo para el siguiente ciclo
+            -- Actualización de registros
             v_n_minus_1 <= i_Vi;
-            i_n_minus_1 <= i_n_reg;
+            i_n_minus_1 <= i_n_next;
+            i_n_reg <= i_n_next;
         end if;
     end process;
-
+    
     -- Asignación de la salida
     o_IL <= i_n_reg;
 
