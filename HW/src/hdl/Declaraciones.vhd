e03a6a5611621e5f1987533df7fec98d3b1f9810
LucasCmb <34749734+lucasCmb-eie@users.noreply.github.com>
2025-09-11 20:07:20 -0300

    Se uso el componente existente RL para crear la carga trifasica, y se agrego un vector de fixed para utilizar en el algoritmo

--- HW/src/hdl/Declaraciones.vhd
+++ HW/src/hdl/Declaraciones.vhd
@@ -1,6 +1,7 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use IEEE.STD_LOGIC_SIGNED.all;
+use ieee.fixed_pkg.all;
 
 -- Se crean dos tipos de datos (matriz y vector) que son arreglos de tamanio a definir por parametros
 -- Los elementos de estos arreglos son de tipo std_logic_vector de tamanio parametrizado
@@ -8,6 +9,7 @@ use IEEE.STD_LOGIC_SIGNED.all;
 package Declaraciones is -- Nombre del package
   type vector is array (integer range <>) of std_logic_vector;
   type matriz is array (integer range <>, integer range <>) of std_logic_vector;
+  type sfvector is array (integer range <>) of sfixed;
 end;
 
 package body declaraciones is
--- HW/src/hdl/RL.vhd
+++ HW/src/hdl/RL.vhd
@@ -1,6 +1,7 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.fixed_pkg.all;
+use work.declaraciones.all;
 
 entity RL is
     generic (
@@ -18,52 +19,64 @@ entity RL is
         i_c_b1    : in  sfixed(INT_BITS-1 downto -FRAC_BITS);
 
         -- Puertos de datos
-        i_Vi    : in  sfixed(INT_BITS-1 downto -FRAC_BITS);
-        o_IL    : out sfixed(INT_BITS-1 downto -FRAC_BITS)
+        i_Vi    : in  vector(1 to 3)(31 downto 0);
+        o_IL    : out vector(1 to 3)(31 downto 0)
     );
 end RL;
 
 architecture Behavioral of RL is
 
-    -- Señales para almacenar los valores del ciclo anterior
-    signal i_n_minus_1 : sfixed(INT_BITS-1 downto -FRAC_BITS) := (others => '0');
-    signal v_n_minus_1 : sfixed(INT_BITS-1 downto -FRAC_BITS) := (others => '0');
-
-    -- Señales para los productos intermedios
     constant PROD_INT_BITS  : integer := 2*INT_BITS;
     constant PROD_FRAC_BITS : integer := 2*FRAC_BITS;
+
+    -- Señales para almacenar los valores del ciclo anterior
+    signal i_n_minus_1 : sfvector(1 to 3)(INT_BITS-1 downto -FRAC_BITS) := (others => (others => '0'));
+    signal v_n_minus_1 : sfvector(1 to 3)(INT_BITS-1 downto -FRAC_BITS) := (others => (others => '0'));
+
+    -- Señal para la conversión de entrada
+    signal  i_Vi_s : sfvector(1 to 3)(INT_BITS-1 downto -FRAC_BITS) := (others => (others => '0'));  
+    
     -- Registro para la salida
-    signal i_n_reg : sfixed(INT_BITS-1 downto -FRAC_BITS) := (others => '0');
+    signal i_n_reg : sfvector(1 to 3)(INT_BITS-1 downto -FRAC_BITS) := (others => (others => '0'));
 
 begin
 
+    -- Conversión de std_logic_vector a sfixed para la entrada
+    i_Vi_s(1) <= to_sfixed(i_Vi(1), INT_BITS-1, -FRAC_BITS);
+    i_Vi_s(2) <= to_sfixed(i_Vi(2), INT_BITS-1, -FRAC_BITS);
+    i_Vi_s(3) <= to_sfixed(i_Vi(3), INT_BITS-1, -FRAC_BITS);
+
+    --Conversion de sfixed a std_logic_vector para la salida
+    o_IL(1) <= to_slv(i_n_reg(1));
+    o_IL(2) <= to_slv(i_n_reg(2));
+    o_IL(3) <= to_slv(i_n_reg(3));
+
     process (i_clk, i_rst)
-        variable term_a0_v_n   : sfixed(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
-        variable term_a1_v_n_1 : sfixed(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
-        variable term_b1_i_n_1 : sfixed(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
-        variable i_n_next      : sfixed(INT_BITS-1 downto -FRAC_BITS);
+        variable term_a0_v_n   : sfvector(1 to 3)(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
+        variable term_a1_v_n_1 : sfvector(1 to 3)(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
+        variable term_b1_i_n_1 : sfvector(1 to 3)(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
+        variable i_n_next      : sfvector(1 to 3)(INT_BITS-1 downto -FRAC_BITS);
     begin
         if i_rst = '1' then
-            v_n_minus_1 <= (others => '0');
-            i_n_minus_1 <= (others => '0');
-            i_n_reg <= (others => '0');
+            v_n_minus_1 <= (others => (others => '0'));
+            i_n_minus_1 <= (others => (others => '0'));
+            i_n_reg <= (others => (others => '0'));
         elsif rising_edge(i_clk) then
-            -- Multiplicaciones usando variables
-            term_a0_v_n   := i_c_a0 * i_Vi;
-            term_a1_v_n_1 := i_c_a1 * v_n_minus_1;
-            term_b1_i_n_1 := i_c_b1 * i_n_minus_1;
+            for i in 1 to 3 loop
+                -- Multiplicaciones usando variables
+                term_a0_v_n(i)   := i_c_a0 * i_Vi_s(i);
+                term_a1_v_n_1(i) := i_c_a1 * v_n_minus_1(i);
+                term_b1_i_n_1(i) := i_c_b1 * i_n_minus_1(i);
 
-            -- Suma y resta
-            i_n_next := resize(term_a0_v_n + term_a1_v_n_1 - term_b1_i_n_1, INT_BITS-1, -FRAC_BITS);
+                -- Suma y resta
+                i_n_next(i) := resize(term_a0_v_n(i) + term_a1_v_n_1(i) - term_b1_i_n_1(i), INT_BITS-1, -FRAC_BITS);
 
-            -- Actualización de registros
-            v_n_minus_1 <= i_Vi;
-            i_n_minus_1 <= i_n_next;
-            i_n_reg <= i_n_next;
+                -- Actualización de registros
+                v_n_minus_1(i) <= i_Vi_s(i);
+                i_n_minus_1(i) <= i_n_next(i);
+                i_n_reg(i)     <= i_n_next(i);
+            end loop;
         end if;
     end process;
-    
-    -- Asignación de la salida
-    o_IL <= i_n_reg;
 
 end Behavioral;
