cdd2e1aeced47b51fbd56821b61defa4b6bf1c36
LucasCmb <34749734+lucasCmb-eie@users.noreply.github.com>
2025-11-08 15:02:08 -0300

    Se creo un componente de RL monofasico y se implementa por cada fase en el wrapper para mayor segmentacion de la carga trifasica

---
+++ HW/src/hdl/RL_fase.vhd
@@ -0,0 +1,69 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use ieee.numeric_std.all;
+use ieee.fixed_pkg.all;
+
+-- Bloque monofásico RL discreto
+-- Ecuación: I[n] = a0 * U[n] + a1 * U[n-1] - b1 * I[n-1]
+entity RL_fase is
+    generic (
+        INT_BITS  : integer := 8;
+        FRAC_BITS : integer := 24
+    );
+    port (
+        i_clk    : in  std_logic;
+        i_rst    : in  std_logic;
+        i_enable : in  std_logic;
+
+        i_c_a0 : in sfixed(INT_BITS-1 downto -FRAC_BITS);
+        i_c_a1 : in sfixed(INT_BITS-1 downto -FRAC_BITS);
+        i_c_b1 : in sfixed(INT_BITS-1 downto -FRAC_BITS);
+
+        i_U : in sfixed(INT_BITS-1 downto -FRAC_BITS);
+        o_I : out sfixed(INT_BITS-1 downto -FRAC_BITS)
+    );
+end entity RL_fase;
+
+architecture Behavioral of RL_fase is
+
+    constant PROD_INT_BITS  : integer := 2*INT_BITS;
+    constant PROD_FRAC_BITS : integer := 2*FRAC_BITS;
+
+    signal U_z1 : sfixed(INT_BITS-1 downto -FRAC_BITS) := (others => '0');  -- U[n-1]
+    signal I_z1 : sfixed(INT_BITS-1 downto -FRAC_BITS) := (others => '0');  -- I[n-1]
+    signal I_n  : sfixed(INT_BITS-1 downto -FRAC_BITS) := (others => '0');  -- salida actual
+
+begin
+    process(i_clk, i_rst)
+        variable term_a0_v_n   : sfixed(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
+        variable term_a1_v_n_1 : sfixed(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
+        variable term_b1_i_n_1 : sfixed(PROD_INT_BITS-1 downto -PROD_FRAC_BITS);
+        variable i_n_next      : sfixed(INT_BITS-1 downto -FRAC_BITS);
+    begin
+        if (i_rst = '1') then
+            U_z1 <= (others => '0');
+            I_z1 <= (others => '0');
+            I_n  <= (others => '0');
+
+        elsif rising_edge(i_clk) then
+            if (i_enable = '1') then
+
+                -- Multiplicaciones usando variables
+                term_a0_v_n   := i_c_a0 * i_U;
+                term_a1_v_n_1 := i_c_a1 * U_z1;
+                term_b1_i_n_1 := i_c_b1 * I_z1;
+
+                -- Ecuación de diferencia discreta
+                i_n_next := resize(term_a0_v_n + term_a1_v_n_1 - term_b1_i_n_1, INT_BITS-1, -FRAC_BITS);
+
+                -- Actualización de memorias
+                I_n <= i_n_next;
+                U_z1 <= i_U;
+                I_z1 <= i_n_next;
+            end if;
+        end if;
+    end process;
+
+    o_I <= I_n;
+
+end architecture;
--- HW/src/hdl/wrappers/RL_wrapper.vhd
+++ HW/src/hdl/wrappers/RL_wrapper.vhd
@@ -2,35 +2,40 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use ieee.fixed_pkg.all;
 
-use work.declaraciones.all;
-
 entity RL_wrapper is
+    generic (
+        INT_BITS    : integer := 8;
+        FRAC_BITS   : integer := 24
+    );
     port (
         -- Señales de control
         i_clk     : in  std_logic;
         i_rst   : in  std_logic; -- Reset asíncrono activo a nivel bajo
 
+        o_Enable : out std_logic;
+
         -- Coeficientes del filtro (entradas configurables)
-        i_c_a0    : in  std_logic_vector(31 downto 0);
-        i_c_a1    : in  std_logic_vector(31 downto 0);
-        i_c_b1    : in  std_logic_vector(31 downto 0);
+        i_c_a0    : in  std_logic_vector(INT_BITS -1 + FRAC_BITS downto 0);
+        i_c_a1    : in  std_logic_vector(INT_BITS -1 + FRAC_BITS downto 0);
+        i_c_b1    : in  std_logic_vector(INT_BITS -1 + FRAC_BITS downto 0);
 
         -- Puertos de datos
         i_U : in std_logic_vector(31 downto 0);
         i_V : in std_logic_vector(31 downto 0);
         i_W : in std_logic_vector(31 downto 0);
         
-        o_U : out std_logic_vector(31 downto 0);
-        o_V : out std_logic_vector(31 downto 0);
-        o_W : out std_logic_vector(31 downto 0)
+        o_Iu : out std_logic_vector(31 downto 0);
+        o_Iv : out std_logic_vector(31 downto 0);
+        o_Iw : out std_logic_vector(31 downto 0)
      );
 end RL_wrapper;
 
 architecture Behavioral of RL_wrapper is
 
     signal tick_enable : std_logic;
-    signal s_Ol : vector(1 to 3)(31 downto 0);
-    signal s_Iv : vector(1 to 3)(31 downto 0);
+    signal w_oU : sfixed(INT_BITS-1 downto -FRAC_BITS);
+    signal w_oV : sfixed(INT_BITS-1 downto -FRAC_BITS);
+    signal w_oW : sfixed(INT_BITS-1 downto -FRAC_BITS);
 
 begin
 
@@ -45,31 +50,70 @@ begin
             i_rst  => i_rst,
             o_tick => tick_enable
         );
+    o_Enable <= tick_enable;
 
-    RL_core : entity work.RL
-        generic map (
-            INT_BITS  => 8,
-            FRAC_BITS => 24
-        )
+    RL_U : entity work.RL_fase
+        generic map (INT_BITS => INT_BITS, FRAC_BITS => FRAC_BITS)
         port map (
-            i_clk   => i_clk,
-            i_rst   => i_rst,
+            i_clk => i_clk,
+            i_rst => i_rst,
             i_enable => tick_enable,
-            i_c_a0  => to_sfixed(i_c_a0, 7, -24),
-            i_c_a1  => to_sfixed(i_c_a1, 7, -24),
-            i_c_b1  => to_sfixed(i_c_b1, 7, -24),
-            i_Vi    => s_Iv,
-            o_IL    => s_Ol
+            i_c_a0 => to_sfixed(i_c_a0, 7, -24),
+            i_c_a1 => to_sfixed(i_c_a1, 7, -24),
+            i_c_b1 => to_sfixed(i_c_b1, 7, -24),
+            i_U => to_sfixed(i_U, 7, -24),
+            o_I => w_oU
         );
 
-    --Asignacion de salidas
-    o_U <= s_Ol(1);
-    o_V <= s_Ol(2);
-    o_W <= s_Ol(3);
+    RL_V : entity work.RL_fase
+        generic map (INT_BITS => INT_BITS, FRAC_BITS => FRAC_BITS)
+        port map (
+            i_clk => i_clk,
+            i_rst => i_rst,
+            i_enable => tick_enable,
+            i_c_a0 => to_sfixed(i_c_a0, 7, -24),
+            i_c_a1 => to_sfixed(i_c_a1, 7, -24),
+            i_c_b1 => to_sfixed(i_c_b1, 7, -24),
+            i_U => to_sfixed(i_V, 7, -24),
+            o_I => w_oV
+        );
 
-    --Asignacion de entradas
-    s_Iv(1) <= i_U;
-    s_Iv(2) <= i_V;
-    s_Iv(3) <= i_W;
+    RL_W : entity work.RL_fase
+        generic map (INT_BITS => INT_BITS, FRAC_BITS => FRAC_BITS)
+        port map (
+            i_clk => i_clk,
+            i_rst => i_rst,
+            i_enable => tick_enable,
+            i_c_a0 => to_sfixed(i_c_a0, 7, -24),
+            i_c_a1 => to_sfixed(i_c_a1, 7, -24),
+            i_c_b1 => to_sfixed(i_c_b1, 7, -24),
+            i_U => to_sfixed(i_W, 7, -24),
+            o_I => w_oW
+        );
+
+    o_Iu <= to_slv(w_oU);
+    o_Iv <= to_slv(w_oV);
+    o_Iw <= to_slv(w_oW);
+
+    -- RL_core : entity work.RL
+    --     generic map (
+    --         INT_BITS  => INT_BITS,
+    --         FRAC_BITS => FRAC_BITS
+    --     )
+    --     port map (
+    --         i_clk   => i_clk,
+    --         i_rst   => i_rst,
+    --         i_enable => tick_enable,
+    --         i_c_a0  => to_sfixed(i_c_a0, INT_BITS-1, -FRAC_BITS),
+    --         i_c_a1  => to_sfixed(i_c_a1, INT_BITS-1, -FRAC_BITS),
+    --         i_c_b1  => to_sfixed(i_c_b1, INT_BITS-1, -FRAC_BITS),
+    --         i_U => i_U,
+    --         i_V => i_V,
+    --         i_W => i_W,
+
+    --         o_Iu => o_Iu,
+    --         o_Iv => o_Iv,
+    --         o_Iw => o_Iw
+    --     );
 
 end Behavioral;
